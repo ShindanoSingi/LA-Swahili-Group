fd605b8501ea1df107817aa6b55ff37b4a0fbfef
Shindano Singi <shindanosingi@sekur.com>
2024-02-07 23:04:27 -0500

    Working on About

---
+++ .history/client/src/pages/about/About_20240207225948.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+      return (
+            <div className="mt-[4rem] min-h-[100vh] bg-cyan-700">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Noteworthy technology acquisitions 2021
+                  </h5>
+                  <p class="font-normal text-gray-700 dark:text-gray-400">
+                        Here are the biggest enterprise technology acquisitions
+                        of 2021 so far, in reverse chronological order.
+                  </p>
+            </div>
+      );
+}
+
+export default About;
