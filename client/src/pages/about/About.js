29b86778323cc6ee2e4f073660aff32650baaa60
Shindano Singi <shindanosingi@sekur.com>
2024-06-18 02:33:21 -0400

    fixed about page

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -30,8 +30,8 @@ function About() {
     },[]);
 
       return (
-            <div className=" pt-[4rem] p-2 dark:bg-gray-800 min-h-[100vh]">
-                  <div className="block about-card p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div className=" pt-[4rem] p-2 flex justify-center md:text-lg lg:text-xl dark:bg-gray-800 min-h-[100vh]">
+                  <div className="block about-card max-w-5xl p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                               About Us
                         </h5>
